9cbe57b41f3ca41a16e3eec0044f31c7a6824015
Eden Parker <boline@marshall.edu>
2013-03-07 11:07:21 -0500

    Adds a front page template and modifies the base.php to have the sticky footer and wrapper

--- base.php
+++ base.php
@@ -1,8 +1,6 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
 
-  <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
-
   <?php
     do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
@@ -12,7 +10,7 @@
       get_template_part('templates/header');
     }
   ?>
-
+<div id="wrapper" class="clearfix">
   <div class="wrap container" role="document">
     <div class="content row">
       <div class="main <?php echo roots_main_class(); ?>" role="main">
@@ -24,7 +22,10 @@
       </aside><!-- /.sidebar -->
       <?php endif; ?>
     </div><!-- /.content -->
+   <?php get_template_part('templates/footerwidgets'); ?>
   </div><!-- /.wrap -->
+  <div class="footer-push">&nbsp;</div>
+ </div><!-- /#wrapper -->
 
   <?php get_template_part('templates/footer'); ?>
 
