09916a94558b10cd370dce9bc1f21ba695c6a6a9
Eden Parker <boline@marshall.edu>
2013-03-29 10:08:51 -0400

    Removes the navigation bar if the primary navigation menu does not exist

--- templates/hero.php
+++ templates/hero.php
@@ -4,7 +4,7 @@
     </a></h1>
   <?php  if (is_front_page()) { hero_unit_hook(); } ?>
 </section>
-<nav id="site-nav" class="navbar">
+<?php if ( has_nav_menu( 'primary_navigation' )){ ?><nav id="site-nav" class="navbar">
 <div class="navbar-inner">
   <div class="container">
 <a class="btn btn-navbar" data-toggle="collapse" data-target=".subnav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
@@ -14,3 +14,4 @@
   </div>
   </div>
 </nav>
+<?php } ?>
