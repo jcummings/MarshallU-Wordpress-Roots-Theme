ae78ab4d56bfc042c3f53dbf552b736f25c99057
John Cummings <jcummings@Johns-MacBook-Pro-3.local>
2014-05-30 07:30:18 -0400

    .

--- templates/searchform.php
+++
@@ -1,9 +0,0 @@
-<form role="search" method="get" class="form-search form-inline input-append" action="<?php echo home_url('/'); ?>">
-  <div class="input-group">
-    <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control span2" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
-    <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
-    <span class="input-group-btn">
-      <button type="submit" class="btn"><?php _e('Search', 'roots'); ?></button>
-    </span>
-  </div>
-</form>
